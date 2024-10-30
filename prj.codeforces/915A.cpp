27c11e55439359620acf8e7983c3d05000bdda01
Kuzmina-Karina <kuzminak670@gmail.com>
2024-10-30 10:20:32 +0300

    Update 915A.cpp

--- prj.codeforces/915A.cpp
+++ prj.codeforces/915A.cpp
@@ -3,13 +3,13 @@ int main(){
     int n=0;
     std:: cin >> n;
     int k=0;
-    int ans=0;
+    int ans=1010003;
     std:: cin >> k;
     for (int i=0; i<n; i++){
         int x=0;
         std:: cin >> x;
-        if (k%x==0 and ans<x){
-            ans = x;
+        if (k%x==0 and k/x<ans){
+            ans = k/x;
         }
     }
     std::cout << ans;
