184c2a7dbe611b005af995e9ef5e27aa31de3685
Kuzmina-Karina <kuzminak670@gmail.com>
2024-11-08 20:55:20 +0300

    Create 1669b.cpp

---
+++ prj.codeforces/1669b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+
+int n, a;
+cin >> n;
+for(int i=0; i<n; i++){
+    int f=-1;
+    cin >> a;
+    vector<int> v(a);
+    for(int j=0; j<a; j++){
+        int x;
+        cin >> x;
+        v[x-1]++;
+    }
+    for(int j=0; j<a; j++){
+        if(v[j]>=3){
+            f=j+1;
+            break;
+        }
+    }
+    cout << f << endl;
+}
+
+return 0;
+}
