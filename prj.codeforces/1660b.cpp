459d6da5a1dd3b90da6cbc03f243ecc595654edd
Kuzmina-Karina <kuzminak670@gmail.com>
2024-11-08 21:06:16 +0300

    Create 1660b.cpp

---
+++ prj.codeforces/1660b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+
+int n;
+cin >> n;
+while(n--){
+    int g, a;
+    int m1=0;
+    int m2=0;
+    cin >> g;
+    for(int i=0; i<g; i++){
+        cin >> a;
+        if(a>m1 && a>m2){
+            m2=m1;
+            m1=a;
+        }
+        else
+            if(a>m2 && a<=m1){
+                m2=a;
+            }
+    }
+    if(abs(m1-m2)>1){
+        cout <<"NO" <<endl;
+    }
+    else
+        cout << "YES" << endl;
+
+}
+
+return 0;
+}
